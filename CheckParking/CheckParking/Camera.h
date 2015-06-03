f7c36d1f69db5930dde2d1a6d91ff4036e92ac1a
e125719 <e125719@ie.u-ryukyu.ac.jp>
2015-06-03 19:10:01 +0900

    add Camera.h and Camera.m

---
+++ CheckParking/CheckParking/Camera.h
@@ -0,0 +1,13 @@
+//
+//  Camera.h
+//  CheckParking
+//
+//  Created by e125719 on 2015/06/03.
+//  Copyright (c) 2015年 Takahiro NAGAKURA. All rights reserved.
+//
+
+#ifndef CheckParking_Camera_h
+#define CheckParking_Camera_h
+
+
+#endif
---
+++ CheckParking/CheckParking/Camera.m
@@ -0,0 +1,9 @@
+//
+//  Camera.m
+//  CheckParking
+//
+//  Created by e125719 on 2015/06/03.
+//  Copyright (c) 2015年 Takahiro NAGAKURA. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
