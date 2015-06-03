f947e5a17d149ebddfaecbd1f1b54caa85bb90c5
e125719 <e125719@ie.u-ryukyu.ac.jp>
2015-06-03 19:38:02 +0900

    modify Camera.m to show alert

--- CheckParking/CheckParking/Camera.m
+++ CheckParking/CheckParking/Camera.m
@@ -6,4 +6,18 @@
 //  Copyright (c) 2015年 Takahiro NAGAKURA. All rights reserved.
 //
 
-#import <Foundation/Foundation.h>
+#import "Camera.h"
+
+#define BTN_CAMERA  0
+#define BTN_READ    1
+#define BTN_WRITE   2
+
+@implementation Camera
+
+- (void)showAlert:(NSString *)title text:(NSString *)text {
+    UIAlertView* alert = [[UIAlertView alloc] initWithTitle:title message:text delegate:nil cancelButtonTitle:@"OK" otherButtonTitles:nil];
+    
+    [alert show];
+}
+
+@end
