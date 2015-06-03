1faddc40dfbe83623db388ca966aa144eaa9318c
e125719 <e125719@ie.u-ryukyu.ac.jp>
2015-06-03 19:09:11 +0900

    add AppDelegate.h and AppDelegate.m

---
+++ CheckParking/CheckParking/AppDelegate.h
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.h
+//  CheckParking
+//
+//  Created by e125719 on 2015/06/03.
+//  Copyright (c) 2015年 Takahiro NAGAKURA. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+
+@end
+
