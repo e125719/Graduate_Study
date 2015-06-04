753e0aaa86c682846a7d7fd3056d7710f1a1173d
e125719 <e125719@ie.u-ryukyu.ac.jp>
2015-06-04 18:46:07 +0900

    modify Database.m to operate the database

--- CheckParking/CheckParking/Database.m
+++ CheckParking/CheckParking/Database.m
@@ -69,7 +69,73 @@
     
     // Open the database.
     if (openDatabaseResult == SQLITE_OK) {
-
+        // Declare a sqlite3_stmt object in which will be stored the query after having been compiled into a SQLite statement.
+        sqlite3_stmt *compiledStatement;
+        
+        // Load all data from database to memory or load the affectedRows.
+        int prepareStatementResult = sqlite3_prepare(sqlite3Database, query, -1, &compiledStatement, NULL);
+        if (prepareStatementResult == SQLITE_OK) {
+            // Check if the query is non-executable (select).
+            if (!queryExecutable) {
+                // In this case, data must be loaded from the database.
+                
+                // Declare an array to keep the data for each fetched row.
+                NSMutableArray *arrEachDataRow;
+                
+                // Loop through the results and add them to the results array row by row.
+                while (sqlite3_step(compiledStatement) == SQLITE_ROW) {
+                    // Initialize the mutable array that will contain the data of a fetched row.
+                    arrEachDataRow = [[NSMutableArray alloc] init];
+                    
+                    // Get the total number of columns.
+                    int totalColumns = sqlite3_column_count(compiledStatement);
+                    
+                    // Go through all columns and fetch each column data.
+                    for (int i=0; i<totalColumns; i++) {
+                        // Convert the column data to text (characters).
+                        char *dbDataAsChars = (char *)sqlite3_column_text(compiledStatement, i);
+                        
+                        // If there are contents in the current column (field) then add them to the current row array.
+                        if (dbDataAsChars != NULL) {
+                            // Convert the characters to string.
+                            [arrEachDataRow addObject:[NSString stringWithUTF8String:dbDataAsChars]];
+                        }
+                        
+                        // Keep the current column name.
+                        if (self.arrColumnNames.count != totalColumns) {
+                            dbDataAsChars = (char *)sqlite3_column_name(compiledStatement, i);
+                            [self.arrColumnNames addObject:[NSString stringWithUTF8String:dbDataAsChars]];
+                        }
+                    }
+                    
+                    // Store each fetched data row in the resutls array, but first check if there is actually data.
+                    if (arrEachDataRow.count > 0) {
+                        [self.arrResults addObject:arrEachDataRow];
+                    }
+                }
+            } else {
+                // This is the case of an executable query (insert, update, delete, ...)
+                
+                // Execute the query.
+                int executeQueryResults = sqlite3_step(compiledStatement);
+                if (executeQueryResults == SQLITE_DONE) {
+                    // Keep the affected rows.
+                    self.affectedRows = sqlite3_changes(sqlite3Database);
+                    
+                    // Keep the last inserted row ID.
+                    self.lastInsertedRowID = sqlite3_last_insert_rowid(sqlite3Database);
+                } else {
+                    // If could not execute the query, show the error message on the debugger.
+                    NSLog(@"DB Execute Error: %s", sqlite3_errmsg(sqlite3Database));
+                }
+            }
+        } else {
+            // In the database cannot be opened, then show the error message on the debugger.
+            NSLog(@"DB Open Error: %s", sqlite3_errmsg(sqlite3Database));
+        }
+        
+        // Release the compiled statement from memory.
+        sqlite3_finalize(compiledStatement);
     }
     
     // Close the database.
