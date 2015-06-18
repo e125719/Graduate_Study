50d12fe142ded90331c750654511a4308f509b25
e125719 <e125719@ie.u-ryukyu.ac.jp>
2015-06-18 17:42:06 +0900

    modify database.php to open and close the database

--- Browser/database.php
+++ Browser/database.php
@@ -6,7 +6,26 @@
   </head>
   <body>
     <!-- Web page's body is here -->
-    Here's the database page.
+    Here's the database page.<br \>
+
+    <?php
+      // Open the Database.
+      $mysqli = new mysqli("localhost", "root", "");
+      if ($mysqli->connect_error) {
+        die("Can't Open the Database: " . $mysqli->connect_error);
+      } else {
+        printf("Open the Database Successfully!<br \>");
+      }
+
+      // Close the Database.
+      $closeDB = $mysqli->close();
+      if (!$closeDB) {
+        printf("Can't Close the Database: %s<br \>", $mysqli->error);
+      } else {
+        printf("Close the Database Successfully!<br \>");
+      }
+    ?>
+
     <form name="go_to_index" method="send" action="index.php">
       <input type="button" onclick="location.href='index.php'" value="Go to index">
     </form>
