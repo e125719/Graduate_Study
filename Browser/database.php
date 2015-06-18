ba8d911fb3bd6d7d1b172d0ad118ace4bc53efce
e125719 <e125719@ie.u-ryukyu.ac.jp>
2015-06-18 19:16:22 +0900

    modify database.php to initialize the database

--- Browser/database.php
+++ Browser/database.php
@@ -12,15 +12,41 @@
       // Open the Database.
       $mysqli = new mysqli("localhost", "root", "");
       if ($mysqli->connect_error) {
-        die("Can't Open the Database: " . $mysqli->connect_error);
+        die("Cannot Open the Database: " . $mysqli->connect_error);
       } else {
         printf("Open the Database Successfully!<br \>");
+      }
+
+      // Create the Database.
+      $createDB = $mysqli->query("CREATE DATABASE IF NOT EXISTS CarNumberDB");
+      if (!$createDB) {
+        printf("Cannot Create the Database: %s<br \>", $mysqli->error);
+      } else {
+        printf("Create the Database Successfully!<br \>");
+      }
+
+      // Select the Database.
+      $selectDB = $mysqli->select_db("CarNumberDB");
+      if (!$selectDB) {
+        printf("Cannot Select the Database: %s<br \>", $mysqli->error);
+      } else {
+        printf("Select the Database Successfully!<br \>");
+
+        // Get the Selected Database's Name.
+        $getDBName = $mysqli->query("SELECT DATABASE()");
+        if (!$getDBName) {
+          printf("Cannot Get the Database's Name: %s<br \>", $mysqli->error);
+        } else {
+          $DBName = $getDBName->fetch_row();
+          printf("Selected Database is %s.<br \>", $DBName[0]);
+          $getDBName->close();
+        }
       }
 
       // Close the Database.
       $closeDB = $mysqli->close();
       if (!$closeDB) {
-        printf("Can't Close the Database: %s<br \>", $mysqli->error);
+        printf("Cannot Close the Database: %s<br \>", $mysqli->error);
       } else {
         printf("Close the Database Successfully!<br \>");
       }
