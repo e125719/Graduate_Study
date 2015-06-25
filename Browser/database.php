d307973e36c941520bd609815ad2027c88cbaa8c
e125719 <e125719@ie.u-ryukyu.ac.jp>
2015-06-25 14:18:57 +0900

    modify database.php to delete 'Success' messages and exit the codes if the database is in error

--- Browser/database.php
+++ Browser/database.php
@@ -11,65 +11,41 @@
     <?php
       // Open the Database.
       $mysqli = new mysqli("localhost", "root", "");
-      if ($mysqli->connect_error) {
+      $connectDB = $mysqli->connect_error;
+      if ($connectDB)
         die("Cannot Open the Database: " . $mysqli->connect_error);
-      } else {
-        printf("Open the Database Successfully!<br \>");
-      }
 
       // Create the Database.
       $createDB = $mysqli->query("CREATE DATABASE IF NOT EXISTS CarNumberDB");
-      if (!$createDB) {
-        printf("Cannot Create the Database: %s<br \>", $mysqli->error);
-      } else {
-        printf("Create the Database Successfully!<br \>");
-      }
+      if (!$createDB)
+        die("Cannot Create the Database: " . $mysqli->error);
 
       // Select the Database.
       $selectDB = $mysqli->select_db("CarNumberDB");
-      if (!$selectDB) {
-        printf("Cannot Select the Database: %s<br \>", $mysqli->error);
+      if (!$selectDB)
+        die("Cannot Select the Database: " . $mysqli->error);
+
+      // Create Table in the Database.
+      $createTable = $mysqli->query("CREATE TABLE IF NOT EXISTS CarNumberDB.CarNumber(carNums INTEGER, deps TEXT, attrs TEXT)");
+      if (!$createTable)
+        die("Cannot Create Table in the Database: " . $mysqli->error);
+
+      // Get Datas from the Database.
+      $fetchDB = $mysqli->query("SELECT * from CarNumberDB.CarNumber", MYSQLI_USE_RESULT);
+      if (!$fetchDB) {
+        die("Cannot Get Datas from the Database: " . $mysqli->error);
       } else {
-        printf("Select the Database Successfully!<br \>");
-
-        // Get the Selected Database's Name.
-        $getDBName = $mysqli->query("SELECT DATABASE()");
-        if (!$getDBName) {
-          printf("Cannot Get the Database's Name: %s<br \>", $mysqli->error);
-        } else {
-          $DBName = $getDBName->fetch_row();
-          printf("Selected Database is %s.<br \>", $DBName[0]);
-          $getDBName->close();
-        }
-
-        // Create Table in the Database.
-        $createTable = $mysqli->query("CREATE TABLE IF NOT EXISTS CarNumberDB.CarNumber(carNums INTEGER, deps TEXT, attrs TEXT)");
-        if (!$createTable) {
-          printf("Cannot Create Table in the Database: %s<br \>", $mysqli->error);
-        } else {
-          printf("Create the Table Successfully!<br \>");
-        }
-
-        // Get Datas from Database.
-        $fetchDB = $mysqli->query("SELECT * from CarNumberDB.CarNumber", MYSQLI_USE_RESULT);
-        if (!$fetchDB) {
-          printf("Cannot Get Datas from the Database: %s<br \>", $mysqli->error);
-        } else {
           while ($row = $fetchDB->fetch_row()) {
             printf("%d, %s, %s<br \>", $row[0], $row[1], $row[2]);
           }
 
           $fetchDB->close();
-        }
       }
 
       // Close the Database.
       $closeDB = $mysqli->close();
-      if (!$closeDB) {
-        printf("Cannot Close the Database: %s<br \>", $mysqli->error);
-      } else {
-        printf("Close the Database Successfully!<br \>");
-      }
+      if (!$closeDB)
+        die("Cannot Close the Database: " . $mysqli->error);
     ?>
 
     <form name="go_to_index" method="send" action="index.php">
