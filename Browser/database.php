5b3df0892aba13e829833245cdb2b730bfd0bc8d
e125719 <e125719@ie.u-ryukyu.ac.jp>
2015-06-25 17:00:10 +0900

    fix database.php to show a correct sentence of 'Close MySQL Server'

--- Browser/database.php
+++ Browser/database.php
@@ -47,7 +47,7 @@
       // Close the Database.
       $closeDB = $mysqli->close();
       if (!$closeDB)
-        die("Cannot Close the Database: " . $mysqli->error);
+        die("Cannot Close MySQL Server: " . $mysqli->error);
     ?>
 
     <form name="go_to_index" method="send" action="index.php">
