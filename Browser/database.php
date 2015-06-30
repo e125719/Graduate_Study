bcee7ce3d22c487bc93ea76ff36fb376630d0a0f
e125719 <e125719@ie.u-ryukyu.ac.jp>
2015-06-30 14:46:49 +0900

    modify database.php to use 'try and catch' construction for connecting MySQL server and database

--- Browser/database.php
+++ Browser/database.php
@@ -9,47 +9,18 @@
     Here's the database page.<br \>
 
     <?php
-      // Open the Database.
-      $mysqli = new mysqli("localhost", "root", "");
-      $connectDB = $mysqli->connect_error;
-      if ($connectDB)
-        die("Cannot Open the Database: " . $mysqli->connect_error);
-
-      // Create the Database.
-      $createDB = $mysqli->query("CREATE DATABASE IF NOT EXISTS CarNumberDB");
-      if (!$createDB)
-        die("Cannot Create the Database: " . $mysqli->error);
-
-      // Select the Database.
-      $selectDB = $mysqli->select_db("CarNumberDB");
-      if (!$selectDB)
-        die("Cannot Select the Database: " . $mysqli->error);
-
-      // Create Table in the Database.
-      $createTable = $mysqli->query("CREATE TABLE IF NOT EXISTS CarNumberDB.CarNumber(carNums INTEGER, deps TEXT, attrs TEXT)");
-      if (!$createTable)
-        die("Cannot Create Table in the Database: " . $mysqli->error);
-
-      /*
-      // Get Datas from the Database.
-      $fetchDB = $mysqli->query("SELECT * from CarNumberDB.CarNumber", MYSQLI_USE_RESULT);
-      if (!$fetchDB) {
-        die("Cannot Get Datas from the Database: " . $mysqli->error);
-      } else {
-          while ($row = $fetchDB->fetch_row()) {
-            printf("%d, %s, %s<br \>", $row[0], $row[1], $row[2]);
-          }
-
-          $fetchDB->close();
+      mysqli_report(MYSQLI_REPORT_ALL ^ MYSQLI_REPORT_STRICT);
+      try {
+        $mysqli = new mysqli("localhost", "root", "");                      // Connect MySQL Server.
+        $mysqli->set_charset("utf8");                                       // Choose Character.
+        $mysqli->query("CREATE DATABASE IF NOT EXISTS CarNumberDB");        // Create the Database.
+        $mysqli->select_db("CarNumberDB");                                  // Select the Database.
+        $mysqli->query("CREATE TABLE IF NOT EXISTS 
+          CarNumberDB.CarNumber(carNums INTEGER, deps TEXT, attrs TEXT)");  // Create the Table.
+        $mysqli->close();                                                   // Close MySQL Server.
+      } catch (mysqli_sql_exception $e) {
+        $error = $e->getMessage();
       }
-      */
-
-      /*
-      // Close the Database.
-      $closeDB = $mysqli->close();
-      if (!$closeDB)
-        die("Cannot Close MySQL Server: " . $mysqli->error);
-      */
     ?>
 
     <form name="go_to_index" method="send" action="index.php">
