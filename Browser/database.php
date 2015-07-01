8c9272761e547fcd4bca8fc321196622050e96c1
e125719 <e125719@ie.u-ryukyu.ac.jp>
2015-07-01 18:46:34 +0900

    modify database.php to fetch all datas from the database

--- Browser/database.php
+++ Browser/database.php
@@ -9,7 +9,7 @@
     Here's the database page.<br \>
 
     <?php
-      mysqli_report(MYSQLI_REPORT_ALL ^ MYSQLI_REPORT_STRICT);
+      mysqli_report(MYSQLI_REPORT_ERROR);
       try {
         $mysqli = new mysqli("localhost", "root", "");                      // Connect MySQL Server.
         $mysqli->set_charset("utf8");                                       // Choose Character.
@@ -17,7 +17,6 @@
         $mysqli->select_db("CarNumberDB");                                  // Select the Database.
         $mysqli->query("CREATE TABLE IF NOT EXISTS 
           CarNumberDB.CarNumber(carNums INTEGER, deps TEXT, attrs TEXT)");  // Create the Table.
-        $mysqli->close();                                                   // Close MySQL Server.
       } catch (mysqli_sql_exception $e) {
         $error = $e->getMessage();
       }
@@ -36,6 +35,25 @@
     <form name="go_to_index" method="send" action="index.php">
       <input type="button" value="Go to index" onclick="location.href='index.php'">
     </form>
+
+    <hr>
+
+    <?php
+      $fetch = "SELECT * FROM CarNumberDB.CarNumber";
+      $result = $mysqli->query($fetch, MYSQLI_USE_RESULT);
+      if (!$result) {
+        printf("Cannot Fetch Datas from the Database!: %s<br \>", $mysqli->error);
+        $result->close();
+      } else {
+        while ($row = $result->fetch_row()) {
+          printf("%d, %s, %s<br \>", $row[0], $row[1], $row[2]);
+        }
+
+        $result->close();
+      }
+    ?>
+
+    <hr>
     
     <footer>
     </footer>
