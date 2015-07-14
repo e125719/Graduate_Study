5dcc5ad3995548d419af32836d1234ff7ac1c638
e125719 <e125719@ie.u-ryukyu.ac.jp>
2015-07-14 16:16:10 +0900

    add register.php

---
+++ Browser/register.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+
+<html lang="ja">
+  <head>
+    <link rel="stylesheet" href="style.css">
+    <title>CheckParking - REGISTER</title>
+  </head>
+  <body>
+    <!-- Web page's body is here -->
+    Here's the register page.<br \>
+
+    <footer>
+    </footer>
+  </body>
+</html>
