0562ba5a7b7c9b043ed45fe7f0c7f5f8643c2a98
e125719 <e125719@ie.u-ryukyu.ac.jp>
2015-07-14 14:38:46 +0900

    add MySQL.h and MySQL.m

---
+++ CheckParking/CheckParking/MySQL.h
@@ -0,0 +1,13 @@
+//
+//  MySQL.h
+//  CheckParking
+//
+//  Created by e125719 on 2015/07/14.
+//  Copyright (c) 2015年 Takahiro NAGAKURA. All rights reserved.
+//
+
+#ifndef CheckParking_MySQL_h
+#define CheckParking_MySQL_h
+
+
+#endif
---
+++ CheckParking/CheckParking/MySQL.m
@@ -0,0 +1,9 @@
+//
+//  MySQL.m
+//  CheckParking
+//
+//  Created by e125719 on 2015/07/14.
+//  Copyright (c) 2015年 Takahiro NAGAKURA. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
