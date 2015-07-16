abd2f319f9f7bac951c3ea599630ac3a38e16021
e125719 <e125719@ie.u-ryukyu.ac.jp>
2015-07-16 14:42:18 +0900

    add various files

---
+++ CheckParking/CheckParking/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  CheckParking
+//
+//  Created by e125719 on 2015/07/13.
+//  Copyright (c) 2015年 Takahiro NAGAKURA. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
