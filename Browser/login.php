e35dc05f1ac2c90fb1a309edf5439a4c42fb49b0
e125719 <e125719@ie.u-ryukyu.ac.jp>
2015-07-16 14:40:42 +0900

    modify login.php to use MySQL and search registered datas, for logging in

--- Browser/login.php
+++ Browser/login.php
@@ -9,6 +9,45 @@
     <!-- Web page's body is here -->
     Here's the login page.<br \>
 
+    <?php
+      $hash = "Tx9BcuRZh5FksUq";
+
+      session_start();
+
+      mysqli_report(MYSQLI_REPORT_ERROR);
+      try {
+        $mysqli = new mysqli("localhost", "root", "");                      // Connect MySQL Server.
+        $mysqli->set_charset("utf8");                                       // Choose Character.
+        $mysqli->select_db("LoginDB");                                      // Select the Database.
+      } catch (mysqli_sql_exception $e) {
+        $error = $e->getMessage();
+      }
+
+      $status = "none";
+
+      if (isset($_SESSION["user"])) {
+        $status = "LoggedIn";
+      } elseif (!empty($_POST["tb_user"]) && !empty($_POST["tb_pass"])) {
+        $password = md5($_POST["tb_pass"] . $hash);
+
+        $check = $mysqli->prepare("SELECT * FROM UserInfo WHERE user = ? AND pass = ?");
+        $check->bind_param("ss", $_POST["tb_user"], $password);
+        $check->execute();
+        $check->store_result();
+
+        if ($check->num_rows == 1) {
+          $status = "ok";
+          printf("Success!\n");
+          $_SESSION["user"] = $_POST["tb_user"];
+        } else {
+          $status = "failed";
+          printf("Failed!\n");
+        }
+      }
+
+      session_destroy();
+    ?>
+
     <form name="loginForm" method="POST" action="" id="loginForm_id">
       User:
       <input type="text" name="tb_user" id="tb_user_id" value="" autocomplete="off">
