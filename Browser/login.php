9f4cc4d65f951e04e5c1a71bdf646aa3ba808a1d
e125719 <e125719@ie.u-ryukyu.ac.jp>
2015-07-16 14:39:45 +0900

    modify login.php to put forms for logging in the web page

--- Browser/login.php
+++ Browser/login.php
@@ -9,6 +9,14 @@
     <!-- Web page's body is here -->
     Here's the login page.<br \>
 
+    <form name="loginForm" method="POST" action="" id="loginForm_id">
+      User:
+      <input type="text" name="tb_user" id="tb_user_id" value="" autocomplete="off">
+      Password:
+      <input type="text" name="tb_pass" id="tb_pass_id" value="" autocomplete="off">
+      <input type="submit" name="btn_login" id="btn_login_id" value="Login">
+    </form>
+
     <footer>
     </footer>
   </body>
